bdcf0f89a398819b07c4a11321f830b0aa90c5c5
Marc Steven Clemen <mackyclemen@gmail.com>
2022-06-27 00:22:06 +0800

    Modified PartnersSponsor to include partner name

--- src/components/PartnersSponsor.jsx
+++ src/components/PartnersSponsor.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 
+function PartnerCard({ partnerName, partnerImageUrl, partnerUrl = '#'}) {
+  return (
+    <div>
+      <a href={partnerUrl} rel="noopener noreferrer">
+        <img className="object-contain rounded h-48 w-48 m-auto" src={partnerImageUrl} alt={partnerName} />
+        <p className="text-center mt-4 text-gray-300 text-lg">{partnerName}</p>
+      </a>
+    </div>
+  );
+}
+
 function PartnersSponsor({ title, description, images, titleClass = "" }) {
   return (
     <div className="flex flex-col text-center lg:text-left gap-4">
       <h3 className={`${titleClass} text-3xl font-semibold`}>{title}</h3>
-      <p className="text-gray-300">{description}</p>
-      <div className='flex flex-col lg:flex-row flex-wrap gap-16'>
-        {images.map((image) => <img className="object-contain rounded" src={image.src} alt={image.alt} key={image.src} />)}
+      <div className='flex flex-col lg:flex-row lg:justify-center flex-wrap gap-16'>
+        {images.map((image) => <PartnerCard partnerName={image.alt} partnerImageUrl={image.src} key={image.src}/>)}
       </div>
     </div>
   );
