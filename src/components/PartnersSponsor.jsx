59dfed84741c67b3a9c0d2cad95dfda395d4c8c7
Marc Steven Clemen <mackyclemen@gmail.com>
2022-06-27 01:08:30 +0800

    Partners page rewrite + minor stylistic changes

--- src/components/PartnersSponsor.jsx
+++ src/components/PartnersSponsor.jsx
@@ -5,18 +5,18 @@ function PartnerCard({ partnerName, partnerImageUrl, partnerUrl = '#'}) {
     <div>
       <a href={partnerUrl} rel="noopener noreferrer">
         <img className="object-contain rounded h-48 w-48 m-auto" src={partnerImageUrl} alt={partnerName} />
-        <p className="text-center mt-4 text-gray-300 text-lg">{partnerName}</p>
+        <p className="w-56 text-center mt-4 text-gray-300 text-lg">{partnerName}</p>
       </a>
     </div>
   );
 }
 
-function PartnersSponsor({ title, description, images, titleClass = "" }) {
+function PartnersSponsor({ title, partners, titleClass = "" }) {
   return (
     <div className="flex flex-col text-center lg:text-left gap-4">
       <h3 className={`${titleClass} text-3xl font-semibold`}>{title}</h3>
-      <div className='flex flex-col lg:flex-row lg:justify-center flex-wrap gap-16'>
-        {images.map((image) => <PartnerCard partnerName={image.alt} partnerImageUrl={image.src} key={image.src}/>)}
+      <div className='flex flex-col lg:flex-row lg:justify-center flex-wrap gap-16 mt-4'>
+        {partners.map((partner) => <PartnerCard partnerName={partner.name} partnerImageUrl={partner.imageUrl} key={partner.imageUrl}/>)}
       </div>
     </div>
   );
